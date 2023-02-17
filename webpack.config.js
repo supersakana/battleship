5e6ff94bbe381f1f3220b038e7e15b278db78052
supersakana <zacwilliamson@icloud.com>
2023-02-17 15:55:44 -0600

    installed html-webpack-plugin with html template in the src file, addedd css file and devtool for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+      template: './src/index.html',
+      minify: { collapseWhitespace: false }
+    }),
+  ],
+
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
