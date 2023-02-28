bf6326ea43bc67d41b5e2e0462b5595846992a86
supersakana <zacwilliamson@icloud.com>
2023-02-28 12:11:03 -0600

    refactored validation method, separated each conditional into function

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -1,13 +1,25 @@
+function isWithinBoard(coord) {
+  return coord >= 0 && coord <= 99;
+}
+
+function isSameY(coord, ship, yAxis) {
+  if (ship.isVerti) return coord[0] === yAxis;
+
+  return true;
+}
+
+function isVaccant(coord, board) {
+  return board.at[coord].ship === null;
+}
+
+// public
+
 function isValid(combo, ship, board) {
   const yAxis = combo[0][1];
 
-  if (ship.isVerti) {
-    return combo.every((coord) => coord >= 0 && coord <= 99
-                        && board.at[coord].ship === null
-                        && coord[0] === yAxis);
-  }
-  return combo.every((coord) => coord >= 0 && coord <= 99
-                        && board.at[coord].ship === null);
+  return combo.every((coord) => isWithinBoard(coord)
+                             && isVaccant(coord, board)
+                             && isSameY(coord, ship, yAxis));
 }
 
 export { isValid }; // eslint-disable-line
