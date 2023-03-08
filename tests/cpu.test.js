79e8dd5fadec42e1b9abcbbb192da82e5782f3f3
supersakana <zacwilliamson@icloud.com>
2023-03-08 16:31:30 -0600

    changed player test into cpu test with correct class

--- tests/cpu.test.js
+++ tests/cpu.test.js
@@ -1,4 +1,4 @@
-import Player from '../src/lib/player';
+import Cpu from '../src/lib/cpu';
 import Board from '../src/lib/board';
 import Ship from '../src/lib/ship';
 
@@ -6,56 +6,56 @@ import Ship from '../src/lib/ship';
 
 describe('setGuesses', () => {
   test('takes a coord of hit ship and returns array of coords to hit next', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(3);
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '32', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '32', 'player', mockDisplay);
     cpu.setGuesses('32', foeBoard);
     expect(cpu.guesses).toEqual(['31', '42', '33', '22']);
   });
 
   test('returns array of coords within board parameters', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(3);
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '94', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '94', 'player', mockDisplay);
     cpu.setGuesses('94', foeBoard);
     expect(cpu.guesses).toEqual(['93', '95', '84']);
   });
 
   test('returns array of coords within board parameters (corner)', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(3);
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '00', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '00', 'player', mockDisplay);
     cpu.setGuesses('00', foeBoard);
     expect(cpu.guesses).toEqual(['10', '01']);
   });
 
   test('returns array of coords excluding cells that have been hit', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(3);
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '25', 'human', mockDisplay);
-    foeBoard.receiveAttack('15', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '25', 'player', mockDisplay);
+    foeBoard.receiveAttack('15', 'player', mockDisplay);
     cpu.setGuesses('25', foeBoard);
 
     expect(cpu.guesses).toEqual(['24', '35', '26']);
   });
 
   test('returns filtered guesses if hit pattern is vertical', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(3);
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '14', 'human', mockDisplay);
-    foeBoard.receiveAttack('14', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '14', 'player', mockDisplay);
+    foeBoard.receiveAttack('14', 'player', mockDisplay);
     cpu.setGuesses('14', foeBoard);
-    foeBoard.receiveAttack('15', 'human', mockDisplay);
+    foeBoard.receiveAttack('15', 'player', mockDisplay);
     cpu.guesses.splice(cpu.guesses.indexOf('15'), 1); // temporary
     cpu.setGuesses('15', foeBoard);
 
@@ -64,15 +64,15 @@ describe('setGuesses', () => {
   });
 
   test('returns filtered guesses if hit pattern is horizontal', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(3);
     ship.isVerti = false;
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '53', 'human', mockDisplay);
-    foeBoard.receiveAttack('53', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '53', 'player', mockDisplay);
+    foeBoard.receiveAttack('53', 'player', mockDisplay);
     cpu.setGuesses('53', foeBoard);
-    foeBoard.receiveAttack('63', 'human', mockDisplay);
+    foeBoard.receiveAttack('63', 'player', mockDisplay);
     cpu.guesses.splice(cpu.guesses.indexOf('63'), 1); // temporary
     cpu.setGuesses('63', foeBoard);
 
@@ -81,13 +81,13 @@ describe('setGuesses', () => {
   });
 
   test('once a ship is sunk, the guesses and last hit are set to null', () => {
-    const cpu = new Player('cpu');
+    const cpu = new Cpu('cpu');
     const foeBoard = new Board();
     const ship = new Ship(2);
     const mockDisplay = jest.fn();
-    foeBoard.placeShip(ship, '71', 'human', mockDisplay);
-    foeBoard.receiveAttack('71', 'human', mockDisplay);
-    foeBoard.receiveAttack('72', 'human', mockDisplay);
+    foeBoard.placeShip(ship, '71', 'player', mockDisplay);
+    foeBoard.receiveAttack('71', 'player', mockDisplay);
+    foeBoard.receiveAttack('72', 'player', mockDisplay);
     cpu.setGuesses('72', foeBoard);
 
     expect(ship.isSunk).toBeTruthy();
